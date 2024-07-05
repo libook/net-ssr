f49fa7f1fc88c1c9bd970b361a9176ed472cbe94
libook <libook7@gmail.com>
2024-07-05 13:12:29 +0800

    build: generate man pages only with NET_SSR_BUILD_WITH_MAN=1

--- build.rs
+++ build.rs
@@ -1,28 +1,31 @@
 fn main() -> std::io::Result<()> {
-    // Check if ./man directory exists, if not create it
-    let man_dir =std::path::Path::new("./man");
-    if !man_dir.exists() {
-        std::fs::create_dir("./man")?;
-    }
+    // Check NET_SSR_BUILD_WITH_MAN
+    if std::env::var("NET_SSR_BUILD_WITH_MAN").is_ok() {
+        // Check if ./man directory exists, if not create it
+        let man_dir = std::path::Path::new("./man");
+        if !man_dir.exists() {
+            std::fs::create_dir("./man")?;
+        }
 
-    {
-        let cmd = net_ssr_shared::get_interrogator_command();
+        {
+            let cmd = net_ssr_shared::get_interrogator_command();
 
-        let man = clap_mangen::Man::new(cmd);
-        let mut buffer: Vec<u8> = Default::default();
-        man.render(&mut buffer)?;
+            let man = clap_mangen::Man::new(cmd);
+            let mut buffer: Vec<u8> = Default::default();
+            man.render(&mut buffer)?;
 
-        std::fs::write(man_dir.join("interrogator.1"), buffer)?;
-    }
+            std::fs::write(man_dir.join("interrogator.1"), buffer)?;
+        }
 
-    {
-        let cmd = net_ssr_shared::get_transponder_command();
+        {
+            let cmd = net_ssr_shared::get_transponder_command();
 
-        let man = clap_mangen::Man::new(cmd);
-        let mut buffer: Vec<u8> = Default::default();
-        man.render(&mut buffer)?;
+            let man = clap_mangen::Man::new(cmd);
+            let mut buffer: Vec<u8> = Default::default();
+            man.render(&mut buffer)?;
 
-        std::fs::write(man_dir.join("transponder.1"), buffer)?;
+            std::fs::write(man_dir.join("transponder.1"), buffer)?;
+        }
     }
 
     Ok(())
